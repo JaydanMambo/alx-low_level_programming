50799de5ac30dfa1ddff3e23287d121d0cf4a68d
JaydanMambo <jdanmambo@gmail.com>
2023-09-07 10:47:48 -0700

    Add script to display size of the various data types in the system

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,17 +5,14 @@
  *
  * Return: Always 0 (Success)
  */
-int main(void)
-{
-    printf("Size of char: %zu\n", sizeof(char));
-    printf("Size of short: %zu\n", sizeof(short));
-    printf("Size of int: %zu\n", sizeof(int));
-    printf("Size of long: %zu\n", sizeof(long));
-    printf("Size of long long: %zu\n", sizeof(long long));
-    printf("Size of float: %zu\n", sizeof(float));
-    printf("Size of double: %zu\n", sizeof(double));
-    printf("Size of long double: %zu\n", sizeof(long double));
 
-    return (0);
+int main(void) {
+	printf("Size of a char: %lu byte(s)\n", sizeof(char));
+	printf("Size of an int: %lu byte(s)\n", sizeof(int));
+	printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %lu byte(s)\n", sizeof(float));
+
+	return (0);
 }
 
