2d353877bad494bd37da0bdaa804e391d91629d6
JaydanMambo <jdanmambo@gmail.com>
2023-09-07 08:17:00 -0700

    Add script hat prints text followed by a new line

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 
 /**
- * Print "Programming is like building a multilingual puzzle and return zero
+ * Print "Programming is like building a multilingual puzzle 
+ * Return 0
  */
-int main (void)
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
