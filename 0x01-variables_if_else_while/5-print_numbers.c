a104f18349e226a6f0252ed0380ed34452124596
JaydanMambo <jdanmambo@gmail.com>
2023-09-08 14:24:21 -0700

    Add script that prints all single digit numbers from 0 to 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main -  print all single digit numbers from 0-10, followed by a new line
+ * Return: - 0 and exit
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 0; n <= 10; n++)
+		printf("%d\n", n);
+	return (0);
+}
