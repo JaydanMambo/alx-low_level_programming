4ce6feb8a9fcceb76e0b95de1ec76b6518d4d42f
JaydanMambo <jdanmambo@gmail.com>
2023-09-08 13:45:39 -0700

    Add script that prints alphabet in lowercase using putchar only

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -10,8 +10,8 @@ int main(void)
 {
 	char letter;
 
-	for (letter = 'a'; letter <= "z"; letter++)
+	for (letter = 'a'; letter <= 'z'; letter++)
 		putchar(letter);
-	putchar("\n");
+	putchar('\n');
 	return (0);
 }
