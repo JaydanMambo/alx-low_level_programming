3ac4fa414c2f0d3f05ce2d4b467b8554a56510ae
JaydanMambo <jdanmambo@gmail.com>
2023-09-13 13:03:17 -0700

    Add a program that tests if function gives the correct output when given a case of 0.

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* main - tests if the function being called gives the correct output for 0
+* Return: 0
+*/
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
