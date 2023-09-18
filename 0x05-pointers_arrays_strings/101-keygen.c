1d8df5ed311d383631f83c514f9c3201197c67fd
JaydanMambo <jdanmambo@gmail.com>
2023-09-18 14:14:03 -0700

    Add a program that generates random valid passwords for the program 101-crackme

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,15 +1,37 @@
-#include "main.h"
+#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <stdio.h>
+
 /**
+ * main - Entry point of the program
+ *
+ * This program generates and prints a sequence of random printable ASCII
+ * characters until their combined ASCII values sum up to a specific target value.
+ *
+ * Return: 0 (Success)
  */
 int main(void)
 {
-int num;
+	int target_sum = 2772;
+	int current_sum = 0;
 
-srand(time(0));
-num = rand();
-printf("%i\n", num);
-return (0);
+	/* Seed the random number generator with the current time */
+	srand(time(NULL));
+
+	while (current_sum < target_sum)
+	{
+		char random_char = rand() % 94 + 33; /* Generate a random printable ASCII character */
+		int ascii_value = (int)random_char;
+
+		if (current_sum + ascii_value <= target_sum)
+		{
+			printf("%c", random_char);
+			current_sum += ascii_value;
+		}
+	}
+
+	printf("\n");
+
+	return (0);
 }
+
