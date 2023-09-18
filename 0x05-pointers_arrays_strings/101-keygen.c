e124fe3195a2d9655a4f4e8b57339ae1b1ac9dcf
JaydanMambo <jdanmambo@gmail.com>
2023-09-18 14:46:36 -0700

    Add a program that generates random valid passwords for the program 101-crackme

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -5,35 +5,35 @@
 
 #define TARGET_CHECKSUM 2772
 
-// Function to generate a random ASCII character within a specified range
+/* Function to generate a random ASCII character within a specified range */
 char getRandomAsciiChar() {
-    return rand() % (126 - 32 + 1) + 32;  // Generates random ASCII characters between 32 (' ') and 126 ('~')
+    return rand() % (126 - 32 + 1) + 32;  /* Generates random ASCII characters between 32 (' ') and 126 ('~') */
 }
 
 int main() {
-    srand(time(NULL));  // Seed the random number generator with the current time
+    srand(time(NULL));  /* Seed the random number generator with the current time */
 
     int currentChecksum = 0;
-    char password[50];  // Adjust the array size as needed
+    char password[50];  /* Adjust the array size as needed */
 
     while (currentChecksum != TARGET_CHECKSUM) {
-        // Generate a random character and add its ASCII value to the checksum
+        /* Generate a random character and add its ASCII value to the checksum */
         char character = getRandomAsciiChar();
         currentChecksum += character;
 
-        // Check if the checksum exceeds the target
+        /* Check if the checksum exceeds the target */
         if (currentChecksum > TARGET_CHECKSUM) {
-            // Reset the checksum and regenerate the password
+            /* Reset the checksum and regenerate the password */
             currentChecksum = 0;
-            password[0] = '\0';  // Reset the password
+            password[0] = '\0';  /* Reset the password */
             continue;
         }
 
-        // Append the character to the password
+        /* Append the character to the password */
         size_t passwordLength = strlen(password);
         if (passwordLength < sizeof(password) - 1) {
             password[passwordLength] = character;
-            password[passwordLength + 1] = '\0';  // Null-terminate the password
+            password[passwordLength + 1] = '\0';  /* Null-terminate the password */
         }
     }
 
