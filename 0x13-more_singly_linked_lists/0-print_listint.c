da78e8d961b0257d4d758fcc326fb4ca9868cb44
JaydanMambo <jdanmambo@gmail.com>
2023-10-24 02:36:10 -0700

    Task 0

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -2,9 +2,9 @@
 #include <stdio.h>
 
 /**
- * print_listint - print elements of linkedlist
- * @h: head
- * Return: length of a string
+ * print_listint - Prints all the elements of a listint_t list
+ * @h:A pointer to the head of the list
+ * Return: The number of nodes in the list
  */
 
 size_t print_listint(const listint_t *h)
